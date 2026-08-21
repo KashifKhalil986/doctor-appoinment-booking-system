532ca9585e9f68a70bcb0984b1cf2e0adbc58937
KashifKhalil986 <kashifkhalil720@gmail.com>
2026-08-21 18:34:29 +0500

    created add-doctor ,all-appoinment ,dashboard, doctor-list component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import "./App.css";
 import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Admin/Dashboard";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import AllAppoinment from "./pages/Admin/AllAppoinment";
+import DoctorList from "./pages/Admin/DoctorList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -15,6 +19,13 @@ const App = () => {
         <Navbar />
         <div className="flex items-start">
           <Sidebar />
+          <Routes>
+            <Route path="/" element={<></>} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/add-doctor" element={<AddDoctor />} />
+            <Route path="/all-appoinment" element={<AllAppoinment />} />
+            <Route path="/doctor-list" element={<DoctorList />} />
+          </Routes>
         </div>
       </div>
     </>
---
+++ admin/src/pages/Admin/AllAppoinment.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllAppoinment = () => {
+  return <div>AllAppoinment</div>;
+};
+
+export default AllAppoinment;
