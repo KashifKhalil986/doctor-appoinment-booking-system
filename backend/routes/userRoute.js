b10484ec9ac41db03f6e3973dda8c5f9e9a34a6c
KashifKhalil986 <kashifkhalil720@gmail.com>
2026-09-01 18:04:41 +0500

    created APIs for book,cancel and list appointment

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,11 @@
 import express from "express";
 import {
+  bookAppointment,
+  cancelAppointment,
   getProfile,
+  listAppointment,
   loginUser,
+  makePayment,
   registerUser,
   updateProfile,
 } from "../controllers/userController.js";
@@ -13,5 +17,9 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post("/update-profile", authUser, updateProfile);
+userRouter.post("/book-appointment", authUser, bookAppointment);
+userRouter.get("/appointments", authUser, listAppointment);
+userRouter.post("/cancel-appointment", authUser, cancelAppointment);
+userRouter.post("/make-payment", authUser, makePayment);
 
 export default userRouter;
